a3a6255a789a130482e88761a59b9f03a9d40b15
Stella <stella.sijiacai@gmail.com>
2020-10-29 16:52:41 -0400

    three buttons and links to respective dashes

---
+++ client/src/Login.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+
+export default class Login extends React.Component{
+    render(){
+        return (
+            <div>
+                <h1>Login</h1>
+                <form>
+                    <div>
+                        <p>Username:</p>
+                        <input type="text" name="username" />
+                    </div>
+                    <div>
+                        <p>Password:</p>
+                        <input type="password" name="password" />
+                    </div>
+                    <Link to="/users"><button>User</button></Link>
+                    <Link to="/recommenders"><button>Recommender</button></Link>
+                    <Link to="/reviewers"><button>Reviewer</button></Link>
+                </form>
+            </div>
+        );
+    }
+}
