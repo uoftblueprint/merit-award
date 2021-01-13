c3ec16ea6515171bc6e0725fa1c1270580fd3ee3
Alex Shih <alex.alex.shih@gmail.com>
2021-01-13 16:19:32 -0500

    made usertype many to many relationship

--- server-python/meritServer/users/models.py
+++ server-python/meritServer/users/models.py
@@ -5,16 +5,30 @@ from django.dispatch import receiver
 from pdb import set_trace
 
 
+class UserType(models.Model):
+  STUDENT = 1
+  RECOMMENDER = 2
+  COUNSELOR = 3
+  REVIEWER = 4
+  ADMIN = 5
+  ROLE_CHOICES = (
+      (STUDENT, 'student'),
+      (RECOMMENDER, 'recommender'),
+      (COUNSELOR, 'counselor'),
+      (REVIEWER, 'reviewer'),
+      (ADMIN, 'admin'),
+  )
+
+  id = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, primary_key=True)
+
+  def __str__(self):
+      return self.get_id_display()
+
+
 class MeritUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     updated_at = models.DateTimeField(auto_now=True)
-
-    # user types
-    student = models.BooleanField(default=False)
-    recommender = models.BooleanField(default=False)
-    counselor = models.BooleanField(default=False)
-    reviewer = models.BooleanField(default=False)
-    admin = models.BooleanField(default=False)
+    user_types = models.ManyToManyField(UserType)
 
 @receiver(post_save, sender=User)
 def create_merit_user(sender, instance, created, **kwargs):
--- server-python/meritServer/users/serializers.py
+++ server-python/meritServer/users/serializers.py
@@ -2,7 +2,11 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from pdb import set_trace
 
-from .models import MeritUser
+from .models import MeritUser, UserType
+
+
+class UserTypeSerializer(serializers.ModelSerializer):
+  model = UserType
 
 
 class MeritUserSerializer(serializers.ModelSerializer):
@@ -20,10 +24,11 @@ class MeritUserSerializer(serializers.ModelSerializer):
         style={'input_type': 'password', 'placeholder': 'Password'},
         source="user.password"
     )
+    user_types = UserTypeSerializer(read_only=True, many=True)
 
     class Meta:
         model = MeritUser
-        fields = ("username", "email", "password", "student", "recommender", "counselor", "reviewer", "admin")
+        fields = ("username", "email", "password", "user_types")
 
     def create(self, validated_data):
         user_data = validated_data["user"]
