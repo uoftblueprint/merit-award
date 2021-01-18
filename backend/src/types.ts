c513b788c0558316eadd5a0c4606c274d7361d44
Stella Cai <stella.sijiacai@gmail.com>
2021-01-18 11:24:24 -0500

    started schema of diff user types

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -1,6 +1,33 @@
-import { Document } from "mongoose";
+import { Document, Schema } from "mongoose";
 
 export interface User extends Document {
     email: string;
     password: string;
+    student: Schema.Types.ObjectId;
+    recommender: Schema.Types.ObjectId;
+    reviewer: Schema.Types.ObjectId;
+    counselor: Schema.Types.ObjectId;
+    admin: Schema.Types.ObjectId;
+}
+
+// user types
+export interface Student extends Document {
+    user: Schema.Types.ObjectId;
+    // other data for Student dashboard goes here
+}
+
+export interface Recommender extends Document {
+    user: Schema.Types.ObjectId;
+}
+
+export interface Reviewer extends Document {
+    user: Schema.Types.ObjectId;
+}
+
+export interface Counselor extends Document {
+    user: Schema.Types.ObjectId;
+}
+
+export interface Admin extends Document {
+    user: Schema.Types.ObjectId;
 }
