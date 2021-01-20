20c6734c40c464718fea7a0cc3b0e725584a91fa
Alex Shih <alex.alex.shih@gmail.com>
2021-01-19 22:56:28 -0500

    css for homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,25 +4,29 @@ import "../styles/login.css";
 
 function Home() {
   return (
-    <div className="h-screen bg-blue-50 flex items-center justify-center">
-      <div class="max-w-md flex flex-col p-4 sm:w-1/2 lg:w-1/3 border-2 rounded border-black">
-          <div class="flex-1">
-            <h2 class="text-gray-900 text-2xl font-bold leading-snug">
-              Merit Award Bursary Program Application
-            </h2>
+    <div className="h-screen bg-gray-50 flex items-center justify-center">
+      <div class="bg-white text-center max-w-md flex flex-col p-4 sm:w-1/2 lg:w-1/3 border-2 rounded border-black">
+          <h2 class="text-gray-900 text-2xl font-bold leading-snug">
+            Merit Award Bursary Program Application
+          </h2>
+
+          <div class="mt-24 mb-6">
+            <a
+              href="/login"
+              class="w-1/3 m-0 m-auto my-6 px-3 py-3 text-white font-semibold bg-blue-700 rounded-md shadow-sm"
+            >
+              Login
+            </a>
           </div>
-          <a
-            href="/login"
-            class="w-1/3 my-3 mt-6 inline-flex items-center px-3 py-3 text-white font-semibold bg-blue-700 rounded-md shadow-sm"
-          >
-            Login
-          </a>
-          <a
-            href="/signup"
-            class="w-1/3 my-3 mt-6 inline-flex items-center px-6 py-3 text-white font-semibold bg-blue-700 rounded-md shadow-sm"
-          >
-            Sign Up
-          </a>
+
+          <div class="my-6">
+            <a
+              href="/signup"
+              class="w-1/3 m-0 m-auto my-3 mt-6 px-3 py-3 text-white font-semibold bg-blue-700 rounded-md shadow-sm"
+            >
+              Sign Up
+            </a>
+          </div>
         </div>
     </div>
   );
