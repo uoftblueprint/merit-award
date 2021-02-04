3c3caa71ac0f3c495feef77085b0788506347c54
song <songeun.you7@gmail.com>
2021-02-03 22:05:05 -0700

    Add latest style changes

---
+++ client/build/precache-manifest.4f03cbe6a79ae363bc7761294b74ac75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a7234b9851ac6053ba89677439baf06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5dc8c72154bd4d6b1a09",
+    "url": "/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "d38d09283a2236a3e3db",
+    "url": "/static/css/main.49e8133a.chunk.css"
+  },
+  {
+    "revision": "5dc8c72154bd4d6b1a09",
+    "url": "/static/js/2.cbac1da4.chunk.js"
+  },
+  {
+    "revision": "38c6e87d1fc2192650153e1a5989ee12",
+    "url": "/static/js/2.cbac1da4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d38d09283a2236a3e3db",
+    "url": "/static/js/main.3ef37903.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
