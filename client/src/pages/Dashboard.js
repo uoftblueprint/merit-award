2de80824f41a377e9cf19f64c2911da86250a9e6
shereezhang <shereezhang@yahoo.ca>
2021-02-18 14:49:02 -0500

    first modal

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -134,17 +134,66 @@ function Menu(props) {
 function Dashboard() {
 
   const [stageIndex, setStageByIndex] = useState(0);
+  const [modalOpen, setModalOpen] = useState(false);
+
+  let backgroundColor = modalOpen ? 'rgba(107, 114, 128)' : 'white';
+  let border = modalOpen ? 'black' : 'rgba(229, 231, 235)';
+
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100 py-12 px-4 sm:px-6 lg:px-8 bg-superlightgray">
+    <div style={{backgroundColor: backgroundColor}} className="min-h-screen flex items-center justify-center bg-gray-100 py-12 px-4 sm:px-6 lg:px-8 bg-superlightgray">
       <div className="max-w-6xl w-full space-y-8 flex items-center flex-column">
         <h2 className="mt-6 text-center text-5xl text-bold text-gray-700">
           Dashboard
         </h2>
-              {stageIndex == 0 && <RenderDashboardStart setStageByIndex={setStageByIndex} />}
-              {stageIndex == 1 && <RenderDashboardMain setStageByIndex={setStageByIndex}/>}
-        </div>
+        {stageIndex == 0 && <RenderDashboardStart setStageByIndex={setStageByIndex} borderColor={border} />}
+        {stageIndex == 1 && <RenderDashboardMain setStageByIndex={setStageByIndex}/>}
+
+        <button onClick={() => setModalOpen(true)} className="group relative flex justify-center py-2 px-4 border border-transparent text-lg font-medium rounded-md text-white bg-blue-300 hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-900">
+          TEST modal popup
+        </button>    
+
+            { modalOpen && 
+            <div class="fixed z-10 inset-0 overflow-y-auto">
+              <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                <div class="fixed inset-0 transition-opacity" aria-hidden="true">
+                  <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+                </div>
+                <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+                <div class="inline-block align-middle bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
+                  <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                    <div class="sm:flex sm:items-start flex flex-col">
+                      <div class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                        <svg class="h-6 w-6 text-red-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+                        </svg>
+                      </div>
+                      <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                        <h3 class="text-2xl leading-6 font-medium text-gray-900" id="modal-headline">
+                          Verify Your Account
+                        </h3>
+                        <div class="mt-2">
+                          <p class="text-md text-gray-500">
+                            Account activation code has been sent to the e-mail address you provided. Please enter it below.
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="flex flex-col items-center justify-center px-4 py-3 sm:px-6 sm:flex">
+                    <button onClick={() => setModalOpen(false)} type="button" class="sm:align-middle inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-900 text-lg font-medium text-white hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-900 sm:ml-3 sm:w-1/2 sm:text-sm">
+                      Confirm
+                    </button>
+                    <button type="button" class="mt-3 sm:align-middle inline-flex justify-center rounded-md  px-4 py-2 bg-white text-lg font-medium text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-1/2 sm:text-sm">
+                      Resend Code
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            }
       </div>
+    </div>
   );
 }
 
