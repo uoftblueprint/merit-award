f5b27c7872627998269a07045abe52a04fa27fd5
song <songeun.you7@gmail.com>
2021-02-18 17:50:04 -0700

    Change returned element check to appStatus

--- client/src/api/status.js
+++ client/src/api/status.js
@@ -6,10 +6,9 @@ export const getAppStatus = async () => {
   try {
       const appStatus = await client.get('/api/app-status');
 
-      // what to submit as user id to get the proper info?
-      // if (!user || user.status != 200 || typeof(user.data) == "string") {
-      //     throw 'Could not get app status';
-      // }
+      if (!appStatus || appStatus.status != 200 || typeof(appStatus.data) == "string") {
+          throw 'Could not get app status';
+      }
       return appStatus.data;
   } catch (err) {
       console.log(err);
