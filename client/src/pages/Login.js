32f0973a65aff983a66c843a42cf95f839b49350
Alex Shih <alex.alex.shih@gmail.com>
2021-03-04 13:10:23 -0500

    updated login page to formik

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,62 +1,53 @@
 import React, { useState } from "react";
-import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { Link, useHistory } from "react-router-dom";
 import "../styles/login.css";
 import { useDispatch } from 'react-redux';
 import { apiLogin } from "../api/auth";
+import { Field, Form, withFormik, ErrorMessage } from "formik";
+import * as Yup from 'yup';
 
 function Login() {
-  const dispatch = useDispatch();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  function validateForm() {
-    return email.length > 0 && password.length > 0;
-  }
-
-  async function handleSubmit(event) {
-    event.preventDefault();
-
-    try {
-      await apiLogin(email, password);
-      dispatch({ type: 'LOGIN' });
-    } catch (err) {
-      console.log(err);
-    }
-  }
-
   return (
     <div className="login">
       <h1 className="bg-gray-500 text-orange"> MERIT AWARD </h1>
-      <Form onSubmit={handleSubmit}>
-        <Form.Group size="lg" controlId="email">
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            autoFocus
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group size="lg" controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </Form.Group>
-        <button
-          block size="lg"
-          className="indigo-button my-3"
-          onClick={!validateForm()}>
-          Login
-        </button>
+      <Form>
+        <div size="lg">
+          <label htmlFor="email" className="block">Email</label>
+          <Field type="text" name="email" placeholder="Email" />
+          <ErrorMessage name={"email"} />
+        </div>
+        <div size="lg">
+          <label htmlFor="password" className="block">Password</label>
+          <Field size="lg" type="password" name="password" placeholder="Password" />
+          <ErrorMessage name={"password"} />
+        </div>
+        <button type="submit" className="indigo-button my-3">Login</button>
       </Form>
       <Link className="outline-button m-auto" to="/signup">Signup</Link>
     </div>
   );
 }
 
-export default Login;
+const LoginValidation = Yup.object().shape({
+  email: Yup
+    .string()
+    .email()
+    .required(),
+  password: Yup
+    .string()
+    .required(),
+})
+
+const LoginFormik = withFormik({
+  handleSubmit: async (values) => {
+    try {
+      await apiLogin(values.email, values.password);
+      dispatchEvent({ type: 'LOGIN '});
+    } catch (err) {
+      console.log('err :>> ', err);
+    }
+  }, validationSchema: LoginValidation
+})(Login)
+
+export default LoginFormik;
